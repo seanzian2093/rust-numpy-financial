1cedca6440464dffe07a108ab0386248c67fd880
seanzian2093 <seanzhang1111@outlook.com>
2024-09-29 01:19:03 -0400

    Add error handling to irr

--- src/irr.rs
+++ src/irr.rs
@@ -1,4 +1,4 @@
-use crate::{float_close, get_vecf64, ParaMap, ATOL, RTOL};
+use crate::{float_close, get_vecf64, Error, ParaMap, Result, ATOL, RTOL};
 /// # Compute the Internal Rate of Return (IRR)
 /// This is the "average" periodically compounded rate of return that gives a net present value of 0.0
 
@@ -16,7 +16,7 @@ use crate::{float_close, get_vecf64, ParaMap, ATOL, RTOL};
 /// use rfinancial::*;
 /// let values: Vec<f64> = vec![-150000.0, 15000.0, 25000.0, 35000.0, 45000.0, 60000.0];
 /// let irr = InternalRateReturn::from_vec(values);
-/// println!("{:#?}'s irr is {:#?}", irr, irr.get());
+/// println!("{:#?}'s irr is {:?}", irr, irr.get());
 /// ```
 /// ## Caveat
 /// * I use Newton-Raphson method to find first `irr` that makes the `npv` of given cash flows 0
@@ -37,22 +37,28 @@ impl InternalRateReturn {
 
     /// Instantiate a `InterestPayment` instance from a hash map with keys of (`values`)
     /// Since [`HashMap`] requires values of same type, we need to wrap into a variant of enum
-    pub fn from_map(map: ParaMap) -> Self {
-        let values = get_vecf64(&map, "values").unwrap();
-        InternalRateReturn { values }
+    pub fn from_map(map: ParaMap) -> Result<Self> {
+        let op = |err: Error| {
+            Error::OtherError(format!(
+                "Failed construct an instance of `InternalRateReturn` from: `{:?}` <- {}",
+                map, err
+            ))
+        };
+        let values = get_vecf64(&map, "values").map_err(|err| op(err))?;
+        Ok(InternalRateReturn { values })
     }
 
-    fn fx(v: &Vec<f64>, x: f64) -> f64 {
+    fn fx(v: &Vec<f64>, x: f64) -> Result<f64> {
         let fx: f64 = v
             .iter()
             .rev()
             .enumerate()
             .map(|(p, c)| c * x.powf(p as f64))
             .sum();
-        fx
+        Ok(fx)
     }
 
-    fn dx(v: &Vec<f64>, x: f64) -> f64 {
+    fn dx(v: &Vec<f64>, x: f64) -> Result<f64> {
         let dx: f64 = v
             .iter()
             .rev()
@@ -63,19 +69,19 @@ impl InternalRateReturn {
                 c * (p + 1.0) * x.powf(p)
             })
             .sum();
-        dx
+        Ok(dx)
     }
 
     // find 1st root
-    fn find_root(v: &Vec<f64>) -> Option<f64> {
+    fn find_root(v: &Vec<f64>) -> Result<Option<f64>> {
         // to re-implement
         let mut x = -0.9;
         let mut iter = 0;
         while iter < 100 {
             // f
-            let f = Self::fx(v, x);
+            let f = Self::fx(v, x)?;
             // d
-            let d = Self::dx(v, x);
+            let d = Self::dx(v, x)?;
             // if d is 0, update x and continue
             if float_close(d, 0.0, RTOL, ATOL) {
                 x += 1.0;
@@ -88,7 +94,7 @@ impl InternalRateReturn {
 
             // if x and x1 are close enough return
             if float_close(x, x1, RTOL, ATOL) {
-                return Some(x1);
+                return Ok(Some(x1));
             };
 
             // otherwise continue the loop - before next iteration, update x and iter
@@ -96,20 +102,20 @@ impl InternalRateReturn {
             iter += 1;
         }
         // if maximum iteration reached, return roots or None
-        None
+        Ok(None)
     }
 
     // fina all possible roots- not used
-    fn _find_roots(v: &Vec<f64>) -> Vec<f64> {
+    fn _find_roots(v: &Vec<f64>) -> Result<Vec<f64>> {
         // to re-implement
         let mut x = -10.0;
         let mut iter = 0;
         let mut roots = Vec::<f64>::new();
         while iter < 100 {
             // f
-            let f = Self::fx(v, x);
+            let f = Self::fx(v, x)?;
             // d
-            let d = Self::dx(v, x);
+            let d = Self::dx(v, x)?;
             // d is 0, update x and continue
             if float_close(d, 0.0, RTOL, ATOL) {
                 x += 1.0;
@@ -132,21 +138,21 @@ impl InternalRateReturn {
         }
 
         // if maximum iteration reached, return roots or None
-        roots
+        Ok(roots)
     }
 
-    fn irr(&self) -> Option<f64> {
+    fn irr(&self) -> Result<Option<f64>> {
         // vec must at lease be of 2 elements
         // - for now check at this function
         if self.values.len() <= 1 {
-            return None;
+            return Ok(None);
         };
         // if signs of all elements of `values` are same, there is no solution
         let all_negative = self.values.iter().all(|&v| v <= 0.0);
         // - including all 0s
         let all_positive = self.values.iter().all(|&v| v > 0.0);
         if all_negative | all_positive {
-            return None;
+            return Ok(None);
         };
 
         // Otherwise we are set to find irr
@@ -172,12 +178,12 @@ impl InternalRateReturn {
         // Some(select_one(eirr))
 
         // For now use find_root, i.e. return one root or none
-        let irr = Self::find_root(&self.values).unwrap() - 1.0;
-        Some(irr)
+        let irr = Self::find_root(&self.values)?.unwrap() - 1.0;
+        Ok(Some(irr))
     }
 
     /// Get the `irr` from an instance of `InternalRateReturn`
-    pub fn get(&self) -> Option<f64> {
+    pub fn get(&self) -> Result<Option<f64>> {
         self.irr()
     }
 }
@@ -191,7 +197,7 @@ mod tests {
     fn test_irr_fx() {
         let c: Vec<f64> = vec![1.0, 2.0, 3.0];
         let x = 2.0;
-        let res = InternalRateReturn::fx(&c, x);
+        let res = InternalRateReturn::fx(&c, x).unwrap();
         // 1*x^2 + 2*x^1 + 3*x^0 ->
         // 1*2^2 + 2*2^1 + 3*2^0 -> 11
         let tgt = 11.0;
@@ -202,7 +208,7 @@ mod tests {
     fn test_irr_dx() {
         let c: Vec<f64> = vec![1.0, 2.0, 3.0];
         let x = 2.0;
-        let res = InternalRateReturn::dx(&c, x);
+        let res = InternalRateReturn::dx(&c, x).unwrap();
         // 1*x^2 + 2*x^1 + 3*x^0 ->
         // 1*2*x^1 + 2*1*x^0 + 0 ->
         // 1*2*2^1 + 2*1*2^0 + 0 ->
@@ -218,8 +224,8 @@ mod tests {
         // - 0.25* x^2 + 1=0 -> x =2 and -2
         let c: Vec<f64> = vec![-0.25, 0.0, 1.0];
 
-        let root = InternalRateReturn::find_root(&c).unwrap();
-        let tgt = InternalRateReturn::fx(&c, root);
+        let root = InternalRateReturn::find_root(&c).unwrap().unwrap();
+        let tgt = InternalRateReturn::fx(&c, root).unwrap();
         let res = 0.0;
         assert!(
             float_close(res, tgt, RTOL, ATOL),
@@ -234,7 +240,7 @@ mod tests {
         // npf.irr([-150000, 15000, 25000, 35000, 45000, 60000])
         // 0.052432888859413884
         let values: Vec<f64> = vec![-150000.0, 15000.0, 25000.0, 35000.0, 45000.0, 60000.0];
-        let res = InternalRateReturn::from_vec(values).get().unwrap();
+        let res = InternalRateReturn::from_vec(values).get().unwrap().unwrap();
         let tgt = 0.052432888859413884;
         assert!(
             float_close(res, tgt, RTOL, ATOL),
@@ -251,7 +257,11 @@ mod tests {
         let values: Vec<f64> = vec![-150000.0, 15000.0, 25000.0, 35000.0, 45000.0, 60000.0];
         let mut map = ParaMap::new();
         map.insert("values".to_string(), ParaType::VecF64(values));
-        let res = InternalRateReturn::from_map(map).get().unwrap();
+        let res = InternalRateReturn::from_map(map)
+            .unwrap()
+            .get()
+            .unwrap()
+            .unwrap();
         let tgt = 0.052432888859413884;
         assert!(
             float_close(res, tgt, RTOL, ATOL),
@@ -259,5 +269,15 @@ mod tests {
             res,
             tgt
         )
+    }
+
+    #[test]
+    fn test_irr_err() {
+        let values: Vec<f64> = vec![-150000.0, 15000.0, 25000.0, 35000.0, 45000.0, 60000.0];
+        let mut map = ParaMap::new();
+        map.insert("Values".to_string(), ParaType::VecF64(values));
+        let res = InternalRateReturn::from_map(map);
+        let cond = res.is_err();
+        assert!(cond);
     }
 }
